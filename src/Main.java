84cfbbe130876fd54ec294d7f7de9038becdea07
mahekmulani291-crypto <mahekmulani291@gmail.com>
2025-12-19 20:31:35 +0530

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,15 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-        // PostgreSQL connection details
-        String url = "jdbc:postgresql://localhost:5432/internship_day1";
-        String user = "postgres";
-        String password = "Mahek@2005";
-
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-
-        try {
-            // Load PostgreSQL JDBC Driver
-            Class.forName("org.postgresql.Driver");
-
-            // Connect to PostgreSQL
-            conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected successfully!");
-
-            // Create a simple query
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery("SELECT version();");
-
-            // Print the result
-            if (rs.next()) {
-                System.out.println("PostgreSQL Version: " + rs.getString(1));
-            }
-
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-
-        } finally {
-            // Close ResultSet
-            try {
-                if (rs != null) rs.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-            // Close Statement
-            try {
-                if (stmt != null) stmt.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-
-            // Close Connection
-            try {
-                if (conn != null) conn.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
         }
     }
 }
